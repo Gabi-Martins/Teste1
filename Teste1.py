cbaea7e3915bbbee615f6becd50c5d3b0646686e
Ubuntu <ubuntu@ip-172-31-55-182.ec2.internal>
2020-10-27 01:36:29 +0000

    arquivo 4

--- Teste1.py
+++ Teste1.py
@@ -2,14 +2,20 @@ import rospy
 from std_msgs.msg import String
 import math
 
-rospy.init_node('test1')
+rospy.init_node('Teste1')
+
+def matricul(mat1):
+      global matr
+      matr=mat1.data
 
 def timerCallBack(event):
+
     msg = String()
     msg.data = '2017016681'
     pub.publish(msg)
     
 pub = rospy.Publisher('/topic1', String, queue_size=1)
 timer = rospy.Timer(rospy.Duration(0.1), timerCallBack)
+sub = rospy.Subscriber('/topic2', String, matricul)
 
 rospy.spin()
